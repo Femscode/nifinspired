d6e9ff77af92a26d16b21dd1b2d33477e281f404
Pelumi <fasanyafemi@gmail.com>
2026-02-08 13:49:37 +0100

    currenncy

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -26,6 +26,7 @@ class StripeController extends Controller
 
     public function createPaymentIntent(Request $request)
     {
+        return 'tesging';
         $amount = $request->input('amount') * 100;
         $currency =  $request->input('currency');
         $email = $request->input('email');
